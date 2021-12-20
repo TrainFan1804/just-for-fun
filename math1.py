479147ac00501c30bc5eda93707cb433e1eb763e
TrainFan1804 <90546070+TrainFan1804@users.noreply.github.com>
2021-12-20 20:08:59 +0100

    math

---
+++ math1.py
@@ -0,0 +1,9 @@
+import math
+
+
+x = math.floor(2.3532535345)  # rounds DOWN to the nearest int
+y = math.ceil(2.3532535345)  # rounds UP to the nearest int
+z = round(2.3532535345)
+print(x)
+print(y)
+print(z)
---
+++ math2.py
@@ -0,0 +1,16 @@
+import math
+
+x = math.factorial(4)  # return the factorial (fakultät) of a number
+print(x)
+
+list = [1, 2, 3, 4]
+print(math.fsum(list))  # return the sum of all items in any iterable
+print(math.prod(list))  # return the product of all item in any iterable
+
+x = math.gcd(2, 4)  # return the greatest common divisor
+print(x)
+
+x = math.pow(2, 4)  # return the value of x to the power of y (potenz)
+print(x)
+
+print(math.pi)
